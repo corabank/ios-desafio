86e5212e361e47af17784c875634c2f7e0f265ac
Bárbara Barone <babi.barone@cora.com.br>
2024-11-03 00:24:08 -0300

    adiciona navigation

---
+++ BankStatement/BankStatement/DesignSystem/Components/NavigationBar/NavigationBar.swift
@@ -0,0 +1,42 @@
+import UIKit
+
+struct NavigationBar {
+    static func apply(to viewController: UIViewController,
+                      title: String,
+                      rightButtonImage: UIImage?,
+                      rightButtonAction: Selector?) {
+        viewController.title = title
+
+        if let navigationController = viewController.navigationController, navigationController.viewControllers.count > 1 {
+            viewController.navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(named: Icons.icChevronLeft.rawValue),
+                                                                              style: .plain,
+                                                                              target: viewController,
+                                                                              action: #selector(viewController.didTapBackButton))
+        }
+
+        if let rightButtonAction = rightButtonAction {
+            let rightButton = UIBarButtonItem(image: rightButtonImage, style: .plain, target: viewController, action: rightButtonAction)
+            viewController.navigationItem.rightBarButtonItem = rightButton
+        }
+
+        if let navigationController = viewController.navigationController {
+            let appearance = UINavigationBarAppearance()
+            appearance.configureWithOpaqueBackground()
+            appearance.backgroundColor = UIColor(named: Colors.grayFour.rawValue)
+            appearance.titleTextAttributes = [
+                NSAttributedString.Key.foregroundColor: UIColor(named: Colors.offBlack.rawValue) ?? .gray,
+                NSAttributedString.Key.font: UIFont(name: "Avenir", size: 14) ?? .systemFont(ofSize: 14)
+            ]
+            navigationController.navigationBar.standardAppearance = appearance
+            navigationController.navigationBar.scrollEdgeAppearance = appearance
+
+            navigationController.navigationBar.tintColor = UIColor(named: Colors.primary.rawValue)
+        }
+    }
+}
+
+extension UIViewController {
+    @objc func didTapBackButton() {
+        navigationController?.popViewController(animated: true)
+    }
+}
--- BankStatement/BankStatement/DesignSystem/Tokens/Colors.swift
+++ BankStatement/BankStatement/DesignSystem/Tokens/Colors.swift
@@ -2,6 +2,7 @@ enum Colors: String {
     case black = "black",
          grayOne = "grayOne",
          grayTwo = "grayTwo",
+         grayFour = "grayFour",
          offBlack = "offBlack",
          primary = "primary",
          secondaryLight = "secondaryLight",
